06e4cd001ab369318eaaecea4e9d1019d8600f47
𝙒𝙖𝙣𝙣𝙤𝙨 <125775675+wanos25@users.noreply.github.com>
2026-05-03 02:46:15 +0300

    Update image_splitter.dart

--- lib/services/image_splitter.dart
+++ lib/services/image_splitter.dart
@@ -1,5 +1,5 @@
+import 'dart:typed_data';
 import 'package:flutter/services.dart';
-
 import 'package:image/image.dart' as img;
 
 import '../models/puzzle_piece.dart';
@@ -7,29 +7,45 @@ import '../models/puzzle_piece.dart';
 class ImageSplitter {
   static Future<List<PuzzlePiece>> split({int gridSize = 3}) async {
     final data = await rootBundle.load("assets/images/tut.png");
-
     final bytes = data.buffer.asUint8List();
 
     final original = img.decodeImage(bytes)!;
-
     final resized = img.copyResize(original, width: 600, height: 600);
 
     final pieceWidth = resized.width ~/ gridSize;
-
     final pieceHeight = resized.height ~/ gridSize;
 
-    List<PuzzlePiece> pieces = [];
+    final marginX = (pieceWidth * 0.22).toInt();
+    final marginY = (pieceHeight * 0.22).toInt();
 
+    List<PuzzlePiece> pieces = [];
     int index = 0;
 
     for (int row = 0; row < gridSize; row++) {
       for (int col = 0; col < gridSize; col++) {
+        int x = col * pieceWidth - marginX;
+        int y = row * pieceHeight - marginY;
+
+        int w = pieceWidth + marginX * 2;
+        int h = pieceHeight + marginY * 2;
+
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+
+        if (x + w > resized.width) {
+          w = resized.width - x;
+        }
+
+        if (y + h > resized.height) {
+          h = resized.height - y;
+        }
+
         final cropped = img.copyCrop(
           resized,
-          x: col * pieceWidth,
-          y: row * pieceHeight,
-          width: pieceWidth,
-          height: pieceHeight,
+          x: x,
+          y: y,
+          width: w,
+          height: h,
         );
 
         final pieceBytes = Uint8List.fromList(img.encodePng(cropped));
