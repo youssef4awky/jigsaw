4ed53c8bf707dbcf004d6b0ae9671dc2c65ac441
𝙒𝙖𝙣𝙣𝙤𝙨 <125775675+wanos25@users.noreply.github.com>
2026-05-03 03:02:08 +0300

    Add row and col properties to PuzzlePiece class

--- lib/models/puzzle_piece.dart
+++ lib/models/puzzle_piece.dart
@@ -1,15 +1,19 @@
-import 'dart:typed_data';
-
 class PuzzlePiece {
-  final Uint8List bytes;
   final String id;
+  final Uint8List bytes;
   final int correctIndex;
+
+  final int row;
+  final int col;
+
   bool isPlaced;
 
   PuzzlePiece({
     required this.id,
     required this.bytes,
     required this.correctIndex,
+    required this.row,
+    required this.col,
     this.isPlaced = false,
   });
 }
